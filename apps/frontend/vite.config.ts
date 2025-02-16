c67a23e7d01424403e86f02a1de75ce50ababdfd
Faisal Husain <faisalhusain1320@gmail.com>
2025-02-16 22:39:10 +0530

    Revert "add :  tailwind v4 added to project"
    This reverts commit 1b7609e850eb1f5e3745319cef45bac3413541b4.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import tailwindcss from '@tailwindcss/vite'
-
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const BACKEND_URL = env.VITE_BACKEND_URL;
 
   return {
-    plugins: [react(),    tailwindcss(),
-    ],
+    plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
