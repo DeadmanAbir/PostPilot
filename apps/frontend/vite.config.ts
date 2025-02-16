8d6b0cb4ed501b13e5881609857d14560df2dbeb
Your Name <abirdutta90990@gmail.com>
2025-02-16 16:11:03 +0530

    alias configuration

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const BACKEND_URL = env.VITE_BACKEND_URL;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: BACKEND_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
