23186f880cd65d4eaf38698058f5e3b6b00abb70
Your Name <abirdutta90990@gmail.com>
2025-02-16 14:04:35 +0530

    add : started http backend in turborepo

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 
 const app = express();
+// @ts-ignore
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
