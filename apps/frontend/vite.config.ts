1b7609e850eb1f5e3745319cef45bac3413541b4
Faisal Husain <faisalhusain1320@gmail.com>
2025-02-16 18:53:25 +0530

    add :  tailwind v4 added to project

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const BACKEND_URL = env.VITE_BACKEND_URL;
 
   return {
-    plugins: [react()],
+    plugins: [react(),    tailwindcss(),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
