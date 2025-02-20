b2d8c8d8abff00a394aa95a296dbd6dea8c69925
Faisal Husain <faisalhusain1320@gmail.com>
2025-02-20 22:17:28 +0530

    fix : @ path configured

--- apps/frontend/src/components/ui/calendar.tsx
+++ apps/frontend/src/components/ui/calendar.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 import { buttonVariants } from "./button";
-import { cn } from "../../lib/utils";
+import { cn } from "@/lib/utils";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
