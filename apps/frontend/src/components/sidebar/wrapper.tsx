cc8b7b25ac9c6661fa6888d117c07ebe50c75eed
Faisal Husain <faisalhusain1320@gmail.com>
2025-03-09 14:59:26 +0530

    fix : color fix

--- apps/frontend/src/components/sidebar/wrapper.tsx
+++ apps/frontend/src/components/sidebar/wrapper.tsx
@@ -12,7 +12,7 @@ export const Wrapper = ({ children }: WrapperProps) => {
   return (
     <aside
       className={cn(
-        'flex left-0 fixed flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50',
+        'flex left-0 fixed flex-col w-[70px] lg:w-60 h-full bg-background border-r  z-50',
         collapsed && 'lg:w-[70px]',
       )}
     >
