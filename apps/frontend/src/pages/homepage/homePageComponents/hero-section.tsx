66ef5a38cfae39020f51034c6d76f662e30bf1fc
Faisal Husain <faisalhusain1320@gmail.com>
2025-03-17 17:20:21 +0530

    fix : css fix

--- apps/frontend/src/pages/homepage/homePageComponents/hero-section.tsx
+++ apps/frontend/src/pages/homepage/homePageComponents/hero-section.tsx
@@ -1,12 +1,29 @@
 import CustomButton from "@/components/custom-button";
 import TileEffect from "./tile-effect";
-
+import {motion} from 'motion/react'
 
 const HeroSection = () => {
   return (
     <div className=" w-full border-b-2 border-dotted">
-      <section className="container mx-auto py-20 text-center  min-h-[90vh] bg-black flex flex-col items-center justify-center relative overflow-hidden border-r-[2px] border-l-2 border-dotted">
+      <section className="container mx-auto py-20 text-center  min-h-[95vh] bg-black flex flex-col items-center justify-center relative overflow-hidden border-r-[2px] border-l-2 border-dotted">
         <TileEffect />
+        <motion.div 
+          className="absolute  size-[500px] mt-20" // Adjusted size and centering
+          animate={{
+            rotate: 360
+          }}
+          transition={{
+            duration: 8,
+            ease: "linear",
+            repeat: Infinity
+          }}
+        >
+          <img 
+            src="/nugget.avif" 
+            alt="" 
+            className="w-full h-full object-contain"
+          />
+        </motion.div>
 
         <h1 className="text-transparent bg-clip-text bg-gradient-to-b font-mono tracking-widest from-white to-zinc-600  md:text-9xl text-8xl">
           Post  Pilot
