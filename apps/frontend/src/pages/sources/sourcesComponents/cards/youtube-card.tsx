7c51a73f7a283711050039eb668e6e0908a4483a
Faisal Husain <faisalhusain1320@gmail.com>
2025-03-22 17:52:59 +0530

    fix : css fix

--- apps/frontend/src/pages/sources/sourcesComponents/cards/youtube-card.tsx
+++ apps/frontend/src/pages/sources/sourcesComponents/cards/youtube-card.tsx
@@ -6,9 +6,9 @@ interface YoutubeCardProps {
 
 export function YoutubeCard({ videoId }: YoutubeCardProps) {
   return (
-    <div className="h-full w-full aspect-video ">
+    <div className="h-full w-full aspect-video  ">
 
-      <iframe width="100%" height="100%" src={videoId} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <iframe width="100%" height="60%" src={videoId} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   );
 }
