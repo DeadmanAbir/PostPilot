455f8af9bbff9d8cfb7f57d261e67d3cbc0cdf23
Faisal Husain <faisalhusain1320@gmail.com>
2025-04-12 12:33:59 +0530

    add : faq added

--- apps/frontend/src/pages/homepage/homePageComponents/faq.tsx
+++ apps/frontend/src/pages/homepage/homePageComponents/faq.tsx
@@ -6,9 +6,12 @@ import {
 } from "@/components/ui/accordion"
 
 const Faq = () => {
-  return (<div className="z-50 max-w-5xl w-full">
+  return (<div className="z-50 max-w-2xl w-full flex flex-col gap-6 my-20">
+    <div className="text-center text-4xl tracking-wider">
+    Frequently Asked Questions
+    </div>
     <Accordion type="single" collapsible>
-      <AccordionItem value="item-1">
+      <AccordionItem value="item-1" className="border-none bg-blue-300/20 backdrop-blur-2xl px-5">
         <AccordionTrigger>Is it accessible?</AccordionTrigger>
         <AccordionContent>
           Yes. It adheres to the WAI-ARIA design pattern.
