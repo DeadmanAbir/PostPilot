d3820274f7b7e2d6f82b76f706721cdfe2793b6b
Your Name <abirdutta90990@gmail.com>
2025-04-18 10:43:58 +0530

    cohere : refactoring changes for deployment

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -20,11 +20,11 @@ app.use(cors());
 
 app.use(
   session({
-    secret: "your-secret-key", // Replace with a secure random string
+    secret: "your-secret-key",
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+      maxAge: 24 * 60 * 60 * 1000,
     },
   })
 );
