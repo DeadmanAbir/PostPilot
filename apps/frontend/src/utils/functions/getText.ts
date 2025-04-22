0d740c5a97671a5c1b76c9088fb41b7e1223c5ef
Faisal Husain <faisalhusain1320@gmail.com>
2025-04-22 18:57:30 +0530

    fix :type error fixed

--- apps/frontend/src/utils/functions/getText.ts
+++ apps/frontend/src/utils/functions/getText.ts
@@ -1,11 +1,15 @@
-export const getTextFromHTML= (html)=> {
-    // Create a temporary element
-    const temp = document.createElement('div');
-    
-    // Set the HTML content
-    temp.innerHTML = html;
-    
-    // Get the text content (removes all HTML tags and preserves only text)
-    return temp.textContent || temp.innerText || '';
-  }
+interface GetTextFromHTML {
+  (html: string): string;
+}
+
+export const getTextFromHTML: GetTextFromHTML = (html) => {
+  // Create a temporary element
+  const temp = document.createElement('div');
+  
+  // Set the HTML content
+  temp.innerHTML = html;
+  
+  // Get the text content (removes all HTML tags and preserves only text)
+  return temp.textContent || temp.innerText || '';
+};
   
