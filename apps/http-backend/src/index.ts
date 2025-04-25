94452cd85c0574d5297e3f6e52d0ef8419309c3c
Your Name <abirdutta90990@gmail.com>
2025-04-26 01:24:14 +0530

    added trust proxy

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -18,7 +18,7 @@ const app: Express = express();
 const port = process.env.PORT || 9000;
 app.use(express.json());
 app.use(cors());
-
+app.set("trust proxy", 1);
 app.use(
   session({
     secret: process.env.SESSION_SECRET || crypto.randomUUID(),
