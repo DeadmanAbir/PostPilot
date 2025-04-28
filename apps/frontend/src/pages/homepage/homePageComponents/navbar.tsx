3f4236bd642343b428ab89f1b7147001159b1dcd
Faisal Husain <faisalhusain1320@gmail.com>
2025-04-28 12:21:22 +0530

    fix : logo fixed

--- apps/frontend/src/pages/homepage/homePageComponents/navbar.tsx
+++ apps/frontend/src/pages/homepage/homePageComponents/navbar.tsx
@@ -7,8 +7,9 @@ const Navbar = () => {
   return (
     <header className=" mx-auto h-16   fixed top-2 w-full    overflow-hidden z-[999]">
       <div className={`flex items-center justify-between  h-full max-w-7xl mx-auto transition-all duration-300 rounded-full  px-8  backdrop-blur-md ${scrolled ? "bg-blue-400/20" :""} `}>
-        <div className="flex items-center gap-2 text-3xl font-bold tracking-wider -translate-x-5">
-        <img src="/logo.png" alt="Loogo"  className="size-20 translate-x-5"/>PostPilot 
+        <div className="flex items-center gap-2 text-3xl font-bold tracking-wider relative  translate-x-8">
+        PostPilot 
+        <img src="/logo.png" alt="Loogo"  className="size-20 absolute -left-16"/>
         </div>
         {/* <div className="flex items-center gap-3 bg-black/20 backdrop-blur-none py-2 px-5 rounded-full border border-accent/90 overflow-hidden">
           <div>
